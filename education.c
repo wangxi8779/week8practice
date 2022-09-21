2eb355159772bedce2352ac7dc3e4584ab8ddb0d
Xi Wang <wangxi8779@hotmail.com>
2022-09-21 22:50:05 +0930

    style

--- education.c
+++ education.c
@@ -2,19 +2,17 @@
 #include <string.h>
 
 int main(void) {
-    int years;
-    printf("enter the number of years you spent in education: ");
-    scanf("%d", &years);
-    char educationLevel[10];
-    if (years >= 0 && years <= 7){
-        strcpy(educationLevel, "Primary");
-    }
-    else if (years >= 8 && years <= 12){
-        strcpy(educationLevel, "Secondary");
-    }
-    else if (years >= 13){
-        strcpy(educationLevel, "Tertiary");
-    }
-    printf("The level of your eduction is: %s level education\n", educationLevel);
-    return 0;
+  int years;
+  printf("enter the number of years you spent in education: ");
+  scanf("%d", &years);
+  char educationLevel[10];
+  if (years >= 0 && years <= 7) {
+    strcpy(educationLevel, "Primary");
+  } else if (years >= 8 && years <= 12) {
+    strcpy(educationLevel, "Secondary");
+  } else if (years >= 13) {
+    strcpy(educationLevel, "Tertiary");
+  }
+  printf("The level of your eduction is: %s level education\n", educationLevel);
+  return 0;
 }
